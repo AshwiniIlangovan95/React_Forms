d6ba57ba98a744379ab36188eb0e14e6130ced7e
AshwiniIlangovan <ashwiniilangovan@gmail.com>
2023-06-03 15:55:49 +0530

    update

---
+++ src/Forms.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+
+class FormsReact extends Component{
+    constructor (props){
+        super(props)
+        this.state={
+            username:'',
+            topic:'',
+            comments:''
+        }
+    }
+    usernameChange= (event)=>{
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    topicChange=(event)=>{
+        this.setState({
+            topic: event.target.value
+        })
+    }
+
+    commentsChange=(event)=>{
+        this.setState({
+            comments: event.target.value
+        })
+    }
+
+    handleSubmit=(event)=>{
+        alert(`${this.state.username} ${this.state.topic} ${this.state.comments} `)
+        event.preventDefault()
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+            <div>
+                
+                UserName:<br/>
+                <input type="text" value={this.state.username} onChange={this.usernameChange} ></input>
+                <br/>
+                Topic: <br/>
+                <select value={this.state.topic} onChange={this.topicChange}>
+                    <option value="React">React</option>
+                    <option value="Angular">Angular</option>
+                    <option value="HTML">HTML</option>
+                    <option value="CSS">CSS</option>
+                    <option value="JavaScript">JavaScript</option>
+                </select>
+                <br/>
+                Comments:<br/>
+                <textarea value={this.state.comments} onChange={this.commentsChange}></textarea><br/>
+                <button type="submit">Submit</button>
+                           
+            </div>
+            </form>
+        )
+    }
+}
+
+export default FormsReact;
